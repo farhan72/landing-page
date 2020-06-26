4c5901e1881bb08f8cfdd9ccfc6de03b5d77c2d1
alfiy <alfi.cakap@gmail.com>
2020-06-26 17:31:32 +0700

    make lazy load image

--- js/main.js
+++ js/main.js
@@ -37,3 +37,29 @@ sr.reveal('.animate-bottom', {
     distance: '25rem',
     delay: 600
 });
+
+// lazy load image
+const preloadImage = (img) => {
+    const src = img.getAttribute("data-src");
+    img.src = src ? src : '';
+}
+function lazyLoad() {
+    const images = document.querySelectorAll("[data-src]");
+    const config = {
+        root: null,
+        roorMargin: '0px',
+        threshold: 0.25
+    }
+
+    const observer = new IntersectionObserver((entries, imageObserver) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                preloadImage(entry.target);
+                imageObserver.unobserve(entry.target);
+            }
+        });
+    }, config);
+
+    images.forEach(image => observer.observe(image));
+}
+lazyLoad();
